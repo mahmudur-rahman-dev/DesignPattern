0ceda1cd7b832305cbd21fa03211dd21c1dbf956
= <mahmudur.rahman.csedu@gmail.com>
2020-10-31 11:18:28 +0600

    updated AnimalPlay class

--- StrategyDesignPattern/AnimalPlay.java
+++ StrategyDesignPattern/AnimalPlay.java
@@ -6,6 +6,18 @@
 package StrategyDesignPattern;
 
 /**
+ * what is strategy pattern?
+ * - Define a family of algorithms,encapsulate each one, and make them
+ *   interchangeable.The strategy pattern lets the algorithm 
+ *   vary independently from clients that use it
+ * 
+ * - when we want to define a class that will have one behavior that is similar to other behaviors in a list.
+ *   example: # i want the class object to be able to choose from :
+ *          -Not flying
+ *          -Fly with Wings
+ *          -Fly Super Fast
+ * - When we need to use one of several behaviors dynamically
+ * 
  *
  * @author promise
  */
@@ -17,6 +29,8 @@ public class AnimalPlay {
         
         System.out.println("Dog: "+sparky.tryToFly());
         System.out.println("Bird: "+tweety.tryToFly());
+        
+        //giving flying ability to dog
         sparky.setFlyingAbility(new ItFlys());
         System.out.println( "Dog: "+sparky.tryToFly());
     }
